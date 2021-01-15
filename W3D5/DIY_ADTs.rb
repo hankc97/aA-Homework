a74a5821d5c09c062c8874221a0a131e7fa8c9e5
Hank <hankchen1997@yahoo.com>
2021-01-15 08:52:38 -0500

    W3D5

---
+++ W3D5/DIY_ADTs.rb
@@ -0,0 +1,60 @@
+class Stack
+    def initialize
+      @stack = []
+    end
+
+    def push(el)
+      @stack << el
+    end
+
+    def pop
+      @stack.pop
+    end
+
+    def peek
+      @stack[-1]
+    end
+end
+
+class Queue 
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue[-1]
+    end
+
+end
+
+class Map
+    attr_reader :array
+    def initialize
+        @array = []
+    end
+
+    def set(key, value)
+        pair_index = @array.index { |pair| pair[0] == key }
+        if pair_index
+            @array[pair_index][1] = value
+        else
+            @array << [key, value]
+        end
+    end
+
+    def delete(key)
+        @array.reject! {|pair| key == pair[0]}
+    end
+
+
+
+
+end
